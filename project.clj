10148e7da0387992315dfbb06234f320547f590a
Sébastien Le Callonnec <sebastien@weblogism.com>
2020-05-31 10:06:38 +0100

    Update license.

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject hodb-inspect "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "GNU Lesser General Public License v3.0"
+            :url "none"
+            :year 2020
+            :key "lgpl-3.0"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.cli "1.0.194"]
                  [org.clojure/java.jdbc "0.7.11"]
